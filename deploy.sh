2fa5ae10f73a00c77910bf0d7dfe427681deb107
zhangyilong <15324683365@163.com>
2021-01-04 14:24:46 +0800

    更新分支名

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source /etc/profile
 cd /biliob_test/biliob_backend
-git checkout master
+git checkout main
 git pull
 mvn -DfailIfNoTests=false -Dtest package
 pid=$(ps -ef | grep biliob | grep .jar | grep -v grep | awk '{print $2}')
